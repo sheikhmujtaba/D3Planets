344b857a1517e2caea2d4472e2ada26e2bca321e
Muhammad Mujtaba <muhammad.mujtaba@centre.edu>
2019-10-08 15:40:18 -0400

    work

--- planets.js
+++ planets.js
@@ -1,3 +1,38 @@
+var Names = ["tay", "bay", "jay"]
+    d3.select("#A1")
+    .append("p")
+    .text(Names);
 
+    d3.select("#A2")
+    .selectAll("span")
+    .data(Names)
+    .enter()
+    .append("span")
+    .text(function(d){
+        return d
+    });
 
- 
+var planetpromise = d3.json("planets.json")
+    planetpromise.then(
+        function(planetdata)
+        {
+            console.log("planetdata", planetdata);
+            drawplanet(planetdata)
+        },
+        function(err)
+    {
+        console.log("fail", err)
+    }
+    )
+var drawplanet= function(planetdata)
+{
+    d3.select("#B3")
+    .selectAll("img")
+    .data(planetdata)
+    .enter()
+    .append("img")
+    .attr("src", function(d)
+         {
+        return d.img;               
+    })
+}
